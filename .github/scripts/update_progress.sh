75b8899e06df29ac97b3c58c6d7fa37b21bcff14
trindady <arthurtrindadedahora@gmail.com>
2026-08-01 20:16:34 -0300

    Corrigindo erros

---
+++ .github/scripts/update_progress.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ROOT="beecrowd"
+
+TOTAL=0
+OUTPUT=""
+
+EMOJIS=("🟢" "🟡" "🔵" "🟣" "🔴" "🟠" "⚪")
+
+INDEX=0
+
+for folder in "$ROOT"/*; do
+
+    if [ -d "$folder" ]; then
+
+        COUNT=$(find "$folder" -type f -name "*.c" | wc -l)
+
+        NAME=$(basename "$folder")
+
+        NAME=$(echo "$NAME" | sed 's/^[0-9-]*//')
+
+        NAME=$(echo "$NAME" | sed 's/ em C//')
+
+        EMOJI=${EMOJIS[$INDEX]}
+
+        OUTPUT="$OUTPUT$EMOJI $NAME .......... $COUNT\n"
+
+        TOTAL=$((TOTAL+COUNT))
+
+        INDEX=$((INDEX+1))
+
+    fi
+
+done
+
+TODAY=$(date +"%d %b %Y")
+
+BLOCK=$(printf "## 📊 Progress\n\n\`\`\`text\n🟢 Beecrowd\n\n${OUTPUT}\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n🏆 Total Solved: ${TOTAL}\n🕒 Last Update: ${TODAY}\n\`\`\`")
+
+awk '
+/<!-- START_PROGRESS -->/{
+print;
+print block;
+skip=1;
+next
+}
+/<!-- END_PROGRESS -->/{
+skip=0
+}
+!skip
+' block="$BLOCK" README.md > README.tmp
+
+mv README.tmp README.md
