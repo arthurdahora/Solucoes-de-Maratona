88ca856d8632e55ebef82e9266b03ccdac36e21b
trindady <arthurtrindadedahora@gmail.com>
2026-08-11 22:09:22 -0300

    Adicionando soluções

---
+++ beecrowd/1- Beginner em C/1009.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string nome;
+    double salario, vendas;
+    
+    cin >> nome >> salario >> vendas;
+    
+    salario = salario + (vendas*0.15);
+    
+    printf("TOTAL = R$%.2f\n", salario);
+    return 0;
+}
